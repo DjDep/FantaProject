0d9f85c0e54317bd4b3a7975fec61b9debb097aa
Damiano <damiand1308@gmail.com>
2022-05-18 15:25:37 +0200

    Firebase3

--- src/main.js
+++ src/main.js
@@ -6,6 +6,20 @@ import 'firebase/compat/firestore'
 import App from './App.vue'
 import router from './router'
 
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyA3o5UL5TFEVqbAqX912ntJahznIqZK6Jo",
+  authDomain: "madama138.firebaseapp.com",
+  databaseURL: "https://madama138-default-rtdb.firebaseio.com",
+  projectId: "madama138",
+  storageBucket: "madama138.appspot.com",
+  messagingSenderId: "963846846243",
+  appId: "1:963846846243:web:03437be760909526677e96"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
 
 Vue.use(VueCompositionAPI)
 
